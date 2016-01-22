d77af41fbeca5720780b7d83aa766ff701f518c1
iyangxuqing <iyangxuqing@qq.com>
2016-01-22 23:51:36 +0800

    chapter 2: Getting Started

--- todos.js
+++ todos.js
@@ -1,4 +1,35 @@
+Todos = new Mongo.Collection('todos');
+
 if (Meteor.isClient) {
+  Template.todos.helpers({
+    todos: function(){
+      return Todos.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.addTodo.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var todoName = $("[name='todoName']").val();
+      Todos.insert({
+        name: todoName,
+        completed: false,
+        createdAt: new Date()
+      });
+      $("[name='todoName']").val('');
+    }
+  });
+
+  Template.todoItem.events({
+    'click .delete-todo': function(event){
+      event.preventDefault();
+      var documentId = this._id;
+      var confirm = window.confirm("Delete this task?");
+      if(confirm){
+        Todos.remove({_id: documentId});
+      }
+    }
+  });
 
 }
 
